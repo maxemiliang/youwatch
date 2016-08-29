93de2a2e3de9a78f0646aee2dd37854f81a3a880
maxemiliang <maxemiliang@gmail.com>
2016-08-29 12:56:00 +0300

    added a vision plugin for views

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const Hapi = require('hapi')
 const server = new Hapi.Server()
 const joi = require('joi')
 const boom = require('boom')
+const nes = require('nes')
 
 server.connection({ port: 1337 })
 
+
+server.register(require('vision'), (err) => {
+  if (err) {
+      console.log('Failed to load vision.')
+  }
+  server.views({
+    engines: { html: require('handlebars') },
+    path: __dirname + '/views'
+  })
+})
+
+server.start((err) => {
+  if (err) throw err
+  console.log('Server running at:' + server.info.uri)
+})
