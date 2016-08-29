3d22d9471c917a3b8552b242573ddb1e0e366f18
maxemiliang <maxemiliang@gmail.com>
2016-08-29 15:41:04 +0300

    creation of room is now working, basic validation and rooms can be joined. Need to add username checks and start to work on actual functionality

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
+'use strict'
+
 const Joi = require('joi')
 const Boom = require('boom')
 const DataStore = require('nedb')
-const db = new DataStore({ filename: '../chat.db', autoload: true })
+const db = new DataStore({ filename: './chat.db', autoload: true })
 
 module.exports = [
   {
@@ -11,6 +13,7 @@ module.exports = [
       reply.view('index')
     }
   },
+
   {
     method: 'POST',
     path: '/room/create',
@@ -22,7 +25,45 @@ module.exports = [
         }
       },
       handler: (request, reply) => {
-        reply.redirect('/room/' + request.payload.roomname)
+        let room = {
+          creator: request.payload.username,
+          roomname: request.payload.roomname,
+          msgs: {},
+          links: []
+        }
+        db.find({roomname: request.payload.roomname}, (err, result) => {
+          if (err) throw err
+          if (result[0] === undefined) {
+            db.insert(room, (err, newRoom) => {
+              if (err) throw err
+              reply.redirect('/room/' + request.payload.roomname)
+            })
+          } else {
+            reply.redirect('/')
+          }
+        })
+      }
+    }
+  },
+
+  {
+    method: 'GET',
+    path: '/room/{roomname}',
+    config: {
+      validate: {
+        params: {
+          roomname: Joi.string().alphanum().min(5).max(10).required()
+        }
+      },
+      handler: (request, reply) => {
+        db.find({roomname: request.params}, (err, room) => {
+          if (err) throw err
+          if (room[0] !== undefined) {
+            reply(room)
+          } else {
+            reply.redirect('/')
+          }
+        })
       }
     }
   }
